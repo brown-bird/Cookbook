ed76d4e9f988a80a5d9e16675f62e5ea979bcaf2
rclay83 <rclay83@gmail.com>
2015-10-03 11:12:26 -0600

    new t-sql examples folder added

---
+++ TSQL/sql_server_2012_T-SQL_fundamentals/single_table_queries.sql
@@ -0,0 +1,17 @@
+-- Single Table Queries, T-SQL Fundamentals ch.2
+
+--USE TSQL2012
+
+/*
+	0. Links
+		a. MSDN reference conventions: https://msdn.microsoft.com/en-us/library/ms177563(v=sql.110)
+	1. Statement logical processing order:
+		1. FROM
+		2. WHERE
+		3. GROUP BY
+		4. HAVING
+		5. SELECT
+		6. ORDER BY
+
+	2. Output order from a query is not guaranteed unless the "Order By" clause is used. 
+*/
