d26d1d186cdd503eb1398940f0722bbbb697a62e
Richard Clay <rclay83@gmail.com>
2020-02-28 19:54:18 -0700

    Collector example working

--- Bacon/src/main/java/HelloWorld.java
+++ Bacon/src/main/java/HelloWorld.java
@@ -1,7 +1,19 @@
+import com.google.common.collect.Lists;
+
+import java.util.stream.IntStream;
+import static com.google.common.collect.MoreCollectors.onlyElement;
+
+
 public class HelloWorld
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        // Filter a collection to one element using Guava Stream Collector
+        Integer one = Lists.<Integer>newArrayList(1, 2, 3, 4).stream()
+                .filter( num -> num < 2)
+                .map(Integer::valueOf)
+                .collect(onlyElement());
+
+        System.out.println(one);
     }
 }
